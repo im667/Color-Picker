731cc84d529b5cc19b8d27f916f535b5c420c9c3
im667 <koh9037@gmail.com>
2022-03-11 18:35:32 +0900

    ADD: FontSet

---
+++ Color-Picker-ProtoType/FontSet.swift
@@ -0,0 +1,31 @@
+//
+//  FontSet.swift
+//  Color-Picker-ProtoType
+//
+//  Created by mac on 2022/03/11.
+//
+
+import UIKit
+
+struct FontSystem {
+    
+    
+    static let display1_R20 = UIFont(name: "NotoSansKR-Regular", size: 20)
+    
+    
+    static let title1_M16 = UIFont(name: "NotoSansKR-Medium", size: 16)
+    static let title2_R16 = UIFont(name: "NotoSansKR-Regular", size: 16)
+    static let title3_M14 = UIFont(name: "NotoSansKR-Medium", size: 14)
+    static let title4_R14 = UIFont(name: "NotoSansKR-Regular", size: 14)
+    static let title5_M12 = UIFont(name: "NotoSansKR-Medium", size: 12)
+    static let title6_R12 = UIFont(name: "NotoSansKR-Regular", size: 12)
+    
+    
+    static let body1_M16 = UIFont(name: "NotoSansKR-Medium", size: 16)
+    static let body2_R16 = UIFont(name: "NotoSansKR-Regular", size: 16)
+    static let body3_R14 = UIFont(name: "NotoSansKR-Regular", size: 14)
+    static let body4_R12 = UIFont(name: "NotoSansKR-Regular", size: 12)
+    
+    
+    static let caption_R10 = UIFont(name: "NotoSansKR-Regular", size: 10)
+}
