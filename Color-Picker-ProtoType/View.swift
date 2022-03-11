61449e5c6c8893c1eb7437f6d0eb04be9b1c4a73
im667 <koh9037@gmail.com>
2022-03-11 17:58:04 +0900

    ADD: MVVM Setting

---
+++ Color-Picker-ProtoType/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  Color-Picker-ProtoType
+//
+//  Created by mac on 2022/03/11.
+//
+
+import Foundation
---
+++ Color-Picker-ProtoType/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Color-Picker-ProtoType
+//
+//  Created by mac on 2022/03/11.
+//
+
+import Foundation
